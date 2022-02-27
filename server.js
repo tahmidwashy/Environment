b032b0a314fae40637cb91b48c6af208535cf276
tahmidwashy <56697135+tahmidwashy@users.noreply.github.com>
2022-02-27 13:53:29 -0500

    asd

--- server.js
+++ server.js
@@ -63,5 +63,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('Server started at http://localhost:'+ port+'.')
 });
-
-console.log(JSON.parse(data)["articles"][1]["urlToImage"]);
